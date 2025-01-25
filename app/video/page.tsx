dfd9c88a6b982db7707f9140b7db7458c7de7b3c
장보영 <1078739@hanwha.com>
2025-01-25 13:32:38 +0900

    5

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -4,10 +4,7 @@ import { useRef, useState } from "react";
 import ReactPlayer from "react-player";
 
 export default function VideoPage() {
-  const playerRef = useRef<ReactPlayer>(null);
-  const playerRef2 = useRef<ReactPlayer>(null);
-  const playerRef3 = useRef<ReactPlayer>(null);
-  const playerRef4 = useRef<ReactPlayer>(null);
+  const playerRef = useRef<HTMLVideoElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isPlaying2, setIsPlaying2] = useState(false);
   const [isPlaying3, setIsPlaying3] = useState(false);
@@ -20,10 +17,12 @@ export default function VideoPage() {
   const speed = 1;
   const isLoopEnabled = false;
   const progressInterval = 1000;
+  console.log(isPlaying);
 
   const handlePlay = (index: number) => {
     if (index === 1) {
       setIsPlaying(true);
+      playerRef.current?.play();
     } else if (index === 2) {
       setIsPlaying2(true);
     } else if (index === 3) {
@@ -36,6 +35,7 @@ export default function VideoPage() {
   const handlePause = (index: number) => {
     if (index === 1) {
       setIsPlaying(false);
+      playerRef.current?.pause();
     } else if (index === 2) {
       setIsPlaying2(false);
     } else if (index === 3) {
@@ -59,7 +59,7 @@ export default function VideoPage() {
 
   return (
     <div>
-      <h1>Version 1.0.4</h1>
+      <h1>Version 1.0.5</h1>
       <div>
         <div>
           <h3>Video Player1</h3>
@@ -73,40 +73,15 @@ export default function VideoPage() {
           <button onClick={() => setSrc("")}>clear</button>
         </div>
         <div>
-          <video width={width} height={height} src={src} controls />
+          <video ref={playerRef} width={width} height={height} src={src} />
         </div>
         <div>
-          <video width={width} height={height} src={src} controls>
+          <video ref={playerRef} width={width} height={height} src={src}>
             <source type="video/webm" />
           </video>
         </div>
       </div>
-      <div>
-        <h3>Video Player1</h3>
-        <input
-          type="text"
-          value={src}
-          onChange={(e) => setSrc(e.target.value)}
-        />
-        <button onClick={() => handlePlay(1)}>Play</button>
-        <button onClick={() => handlePause(1)}>Pause</button>
-        <ReactPlayer
-          ref={playerRef}
-          url={src}
-          width={Number.isNaN(width) ? width : `${width}px`}
-          height={Number.isNaN(height) ? width : `${height}px`}
-          playing={isPlaying}
-          volume={(volume * 1.0) / 100}
-          playbackRate={speed}
-          loop={isLoopEnabled}
-          progressInterval={progressInterval}
-          onPlay={() => handlePlay(1)}
-          onPause={() => handlePause(1)}
-          onEnded={() => handleFinishPlaying(1)}
-          controls
-          playsinline
-        />
-      </div>
+
       <div>
         <h3>Video Player2</h3>
         <input
@@ -117,7 +92,6 @@ export default function VideoPage() {
         <button onClick={() => handlePlay(2)}>Play</button>
         <button onClick={() => handlePause(2)}>Pause</button>
         <ReactPlayer
-          ref={playerRef2}
           url={src}
           width={Number.isNaN(width) ? width : `${width}px`}
           height={Number.isNaN(height) ? width : `${height}px`}
@@ -144,7 +118,6 @@ export default function VideoPage() {
         <button onClick={() => handlePause(3)}>Pause</button>
         <button onClick={() => setSrc("")}>clear</button>
         <ReactPlayer
-          ref={playerRef3}
           url={src}
           width={Number.isNaN(width) ? width : `${width}px`}
           height={Number.isNaN(height) ? width : `${height}px`}
@@ -170,7 +143,6 @@ export default function VideoPage() {
         <button onClick={() => handlePlay(4)}>Play</button>
         <button onClick={() => handlePause(4)}>Pause</button>
         <ReactPlayer
-          ref={playerRef4}
           url={[{ src: src, type: "video/webm" }]}
           width={Number.isNaN(width) ? width : `${width}px`}
           height={Number.isNaN(height) ? width : `${height}px`}
