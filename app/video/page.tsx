99b782d0e3ce1752598d58071c3ffffe9fe9a2dd
장보영 <1078739@hanwha.com>
2025-01-25 11:20:22 +0900

    fix

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -6,9 +6,7 @@ import ReactPlayer from "react-player";
 export default function VideoPage() {
   const playerRef = useRef<ReactPlayer>(null);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [src, setSrc] = useState(
-    "https://aists-qa.hanwhalife.com/blob/hliazrqaitmdct/introduction/voice/20250124/51365639-82d9-4285-bbb7-0c2a1db07e1d.webm?sv=2024-08-04&spr=https%2Chttp&se=2025-01-25T02%3A01%3A09Z&skoid=3fd89acc-1b59-465f-958a-f2ce75594f1d&sktid=38db9aa0-6bc6-4d2a-8dc3-9c9fa38a5fc4&skt=2025-01-25T01%3A54%3A47Z&ske=2025-01-25T02%3A54%3A47Z&sks=b&skv=2024-08-04&sr=b&sp=r&sig=w10%2F1cig3nJoi0zDCR37eeZtxwzv5zccLud5b8aMjF4%3D"
-  );
+  const [src, setSrc] = useState("");
 
   const width = 640;
   const height = 360;
@@ -32,6 +30,61 @@ export default function VideoPage() {
   return (
     <div>
       <div>
+        <h1>Video Player0</h1>
+        <input
+          type="text"
+          value={src}
+          onChange={(e) => setSrc(e.target.value)}
+        />
+        <button onClick={handlePlay}>Play</button>
+        <button onClick={handlePause}>Pause</button>
+        <button onClick={() => setSrc("")}>clear</button>
+        <ReactPlayer
+          ref={playerRef}
+          url={src}
+          width={Number.isNaN(width) ? width : `${width}px`}
+          height={Number.isNaN(height) ? width : `${height}px`}
+          playing={isPlaying}
+          volume={(volume * 1.0) / 100}
+          playbackRate={speed}
+          loop={isLoopEnabled}
+          progressInterval={progressInterval}
+          onPlay={handlePlay}
+          onPause={handlePause}
+          onEnded={handleFinishPlaying}
+          //   controls
+          //   playsinline
+        />
+      </div>
+      <div>
+        <h1>Video Player1</h1>
+        <input
+          type="text"
+          value={src}
+          onChange={(e) => setSrc(e.target.value)}
+        />
+        <button onClick={handlePlay}>Play</button>
+        <button onClick={handlePause}>Pause</button>
+        <button onClick={() => setSrc("")}>clear</button>
+        <ReactPlayer
+          ref={playerRef}
+          url={src}
+          width={Number.isNaN(width) ? width : `${width}px`}
+          height={Number.isNaN(height) ? width : `${height}px`}
+          playing={isPlaying}
+          volume={(volume * 1.0) / 100}
+          playbackRate={speed}
+          loop={isLoopEnabled}
+          progressInterval={progressInterval}
+          onPlay={handlePlay}
+          onPause={handlePause}
+          onEnded={handleFinishPlaying}
+          //   controls
+          playsinline
+        />
+      </div>
+      <div>
+        <h1>Video Player2</h1>
         <input
           type="text"
           value={src}
@@ -52,7 +105,7 @@ export default function VideoPage() {
           onPlay={handlePlay}
           onPause={handlePause}
           onEnded={handleFinishPlaying}
-          //   controls
+          controls
           playsinline
         />
       </div>
