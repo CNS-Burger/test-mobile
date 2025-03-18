4cb7c739186159c43b00aba7226f2f743e399120
pijoh3 <pijoh3@naver.com>
2025-03-18 10:39:34 +0900

    a

---
+++ app/device/page.tsx
@@ -0,0 +1,26 @@
+import { UAParser } from "ua-parser-js";
+
+const isPWA = () => {
+    return (
+        ("standalone" in window.navigator && window.navigator.standalone === true) || // iOS PWA Standalone
+        document.referrer.includes("android-app://") || // Android Trusted Web App
+        ["fullscreen", "standalone", "minimal-ui"].some(
+            (displayMode) => window.matchMedia("(display-mode: " + displayMode + ")").matches
+        )
+    ); // Chrome PWA (supporting fullscreen, standalone, minimal-ui)
+};
+
+export default function Device() {
+    const ua = UAParser();
+    const { browser, os } = ua;
+    const { name: browserName } = browser ?? {};
+    const { name: osName } = os ?? {};
+
+
+
+  return <div>
+    <h1>Browser: {browserName}</h1>
+    <h1>OS: {osName}</h1>
+    <h1>PWA: {isPWA() ? "Yes" : "No"}</h1>
+  </div>;
+}
