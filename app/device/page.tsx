f57e82bfe2a657324150de9e68b12df184892844
pijoh3 <pijoh3@naver.com>
2025-03-18 10:58:03 +0900

    Display Mode 추가

--- app/device/page.tsx
+++ app/device/page.tsx
@@ -2,6 +2,14 @@
 
 import { UAParser } from "ua-parser-js";
 
+
+
+export default function Device() {
+    const ua = UAParser();
+    const { browser, os } = ua;
+    const { name: browserName } = browser ?? {};
+    const { name: osName } = os ?? {};
+
 const isPWA = () => {
     return (
         ("standalone" in window.navigator && window.navigator.standalone === true) || // iOS PWA Standalone
@@ -12,17 +20,21 @@ const isPWA = () => {
     ); // Chrome PWA (supporting fullscreen, standalone, minimal-ui)
 };
 
-export default function Device() {
-    const ua = UAParser();
-    const { browser, os } = ua;
-    const { name: browserName } = browser ?? {};
-    const { name: osName } = os ?? {};
-
-
+const getDisplayMode = () => {
+    const modes = ["fullscreen", "standalone", "minimal-ui", "browser"];
+    for (const mode of modes) {
+        if (window.matchMedia(`(display-mode: ${mode})`).matches) {
+            return mode;
+        }
+    }
+    return "browser"; // 기본값
+};
 
   return <div>
     <h1>Browser: {browserName}</h1>
     <h1>OS: {osName}</h1>
     <h1>PWA: {isPWA() ? "Yes" : "No"}</h1>
+    <h1>Display Mode: {getDisplayMode()}</h1>
+    {/* <h1>Is Mobile: {isMobile() ? "Yes" : "No"}</h1> */}
   </div>;
 }
