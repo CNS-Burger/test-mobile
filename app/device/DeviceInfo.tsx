322dcb5bba21431f607fc45b2e61d712d9dbcef1
pijoh3 <pijoh3@naver.com>
2025-03-18 13:36:10 +0900

    ㄹ

--- app/device/DeviceInfo.tsx
+++ app/device/DeviceInfo.tsx
@@ -5,10 +5,10 @@ import { UAParser } from "ua-parser-js";
 
 export default function Device() {
     const ua = UAParser();
-    const { browser, os } = ua;
+    const { browser, os, device } = ua;
     const { name: browserName } = browser ?? {};
     const { name: osName } = os ?? {};
-
+    const { model, type, vendor } = device ?? {};
 
     const isPWA = useMemo(() => {
         return (
@@ -18,13 +18,10 @@ export default function Device() {
         );
     }, []);
 
-    // const isPWA = window.matchMedia(`(display-mode: standalone)`).matches;
-
     return <div>
         <h1>Browser: {browserName}</h1>
         <h1>OS: {osName}</h1>
-        {/* <h1>PWA: {displayInfo.isPWA ? "Yes" : "No"}</h1> */}
         <h1>isPWA: {isPWA ? "Yes" : "No"}</h1>
-        {/* <h1>Is Mobile: {isMobile() ? "Yes" : "No"}</h1> */}
+        <h1>Device: {model}, {type}, {vendor}</h1>
     </div>;
 }
